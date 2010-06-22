f2732f5913e472c199e5c7642164d21332e69568
Dan Jacob <danjac354@gmail.com>
2010-06-22 13:22:17 +0100

    render csrf token inside DIV

--- flaskext/wtf.py
+++ flaskext/wtf.py
@@ -10,6 +10,7 @@
 """
 import uuid
 
+
 from wtforms.fields import *
 from wtforms.validators import *
 
@@ -17,6 +18,8 @@ from wtforms import Form as BaseForm
 from wtforms import fields, validators, ValidationError
 
 from flask import request, session, current_app
+
+from jinja2 import Markup
 
 __all__  = ['Form', 'ValidationForm', 'fields', 'validators']
 __all__ += fields.__all__
@@ -31,6 +34,9 @@ class Form(BaseForm):
 
     def __init__(self, formdata=None, *args, **kwargs):
 
+        self._request = kwargs.pop('request', request)
+        self._session = kwargs.pop('session', session)
+
         if formdata is None:
             formdata = request.form
 
@@ -38,20 +44,27 @@ class Form(BaseForm):
         self.csrf_enabled = self.csrf_enabled and \
             current_app.config.get('CSRF_ENABLED', True)
         
-        csrf_token = session.get('_csrf_token')
+        csrf_token = self._session.get('_csrf_token')
         if csrf_token is None:
             csrf_token = _generate_csrf_token()
             session['_csrf_token'] = csrf_token
 
         super(Form, self).__init__(formdata, csrf=csrf_token, *args, **kwargs)
+    
+    @property
+    def csrf_token(self):
+        """
+        Renders CSRF field inside a hidden DIV.
+        """
+        return Markup('<div style="display:none;">%s</div>' % self.csrf)
 
     def validate_csrf(self, field):
         if not self.csrf_enabled or request.is_xhr:
             return
-        csrf_token = session.pop('_csrf_token', None)
+        csrf_token = self._session.pop('_csrf_token', None)
         if not field.data or field.data != csrf_token:
             raise ValidationError, "Missing or invalid CSRF token"
 
     def validate_on_POST(self):
-        return request.method == "POST" and self.validate()
+        return self._request.method == "POST" and self.validate()
 
