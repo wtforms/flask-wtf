6377a8237aa9c9a2df145c0894025b6cfec87153
Dan Jacob <danjac354@gmail.com>
2010-07-01 21:47:52 +0100

    doc fixes

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'flask-wtf'
+project = u'Flask-WTF'
 copyright = u'2010, Dan Jacob'
 
 # The version info for the project you're documenting, acts as replacement for
