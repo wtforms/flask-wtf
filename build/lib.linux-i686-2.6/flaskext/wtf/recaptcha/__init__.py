b8c3ea707ae1fe931f4b7eb808af846adbfd9a6a
Dan Jacob <danjac354@gmail.com>
2010-07-15 15:45:36 +0100

    move to 0.3

--- build/lib.linux-i686-2.6/flaskext/wtf/recaptcha/__init__.py
+++
@@ -1,5 +0,0 @@
-import fields
-import validators 
-import widgets
-
-__all__ = fields.__all__ + validators.__all__ + widgets.__all__
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='Flask-WTF',
-    version='0.2',
+    version='0.3',
     url='http://bitbucket.org/danjac/flask-wtf',
     license='BSD',
     author='Dan Jacob',
