e154119a1775b165b159eedead365976183c172b
Anthony Ford <ford.anthonyj@gmail.com>
2012-12-15 11:53:26 -0600

    Version Bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
     name='Flask-WTF',
-    version='0.8',
+    version='0.8.1',
     url='http://github.com/rduplain/flask-wtf',
     license='BSD',
     author='Dan Jacob',
