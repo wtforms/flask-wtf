809c87f3f089d4777cf710ce03dda431dd4e4355
Hsiaoming Yang <lepture@me.com>
2013-03-12 14:12:42 +0800

    add test case for i18n

---
+++ tests/test_i18n.py
@@ -0,0 +1,15 @@
+from __future__ import with_statement
+
+from base import TestCase
+
+
+class TestValidateOnSubmit(TestCase):
+    def test_submitted_and_valid(self):
+        self.app.config['CSRF_ENABLED'] = False
+
+        response = self.client.post(
+            "/",
+            headers={'Accept-Language': 'zh-CN,zh;q=0.8'},
+            data={}
+        )
+        assert '\u8be5\u5b57\u6bb5\u662f' in response.data
