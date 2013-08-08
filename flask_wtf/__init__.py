dd43c08a3f46edf38a4267f5b78ea829fabee571
Hsiaoming Yang <me@lepture.com>
2013-08-08 09:34:50 +0800

    clean up imports. close #35

--- flask_wtf/__init__.py
+++ flask_wtf/__init__.py
@@ -11,30 +11,5 @@
 # flake8: noqa
 from __future__ import absolute_import
 
-from wtforms import fields, widgets, validators
-from wtforms.fields import *
-from wtforms.validators import *
-from wtforms.widgets import *
-
 from .form import Form
-from . import html5
-from . import recaptcha
-
-from .recaptcha.fields import RecaptchaField
-from .recaptcha.widgets import RecaptchaWidget
-from .recaptcha.validators import Recaptcha
-
-fields.RecaptchaField = RecaptchaField
-widgets.RecaptchaWidget = RecaptchaWidget
-validators.Recaptcha = Recaptcha
-
-from .file import FileField
-from .file import FileAllowed, FileRequired
-from .file import file_allowed, file_required
-
-fields.FileField = FileField
-
-validators.file_allowed = file_allowed
-validators.file_required = file_required
-validators.FileAllowed = FileAllowed
-validators.FileRequired = FileRequired
+from .csrf import CsrfProtect
