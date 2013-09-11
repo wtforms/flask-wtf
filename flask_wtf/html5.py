8bfc38173f17c05f6f0b1971466deefd77f0d5cf
Hsiaoming Yang <me@lepture.com>
2013-09-11 16:54:08 +0800

    Remove DateInput. #81

--- flask_wtf/html5.py
+++ flask_wtf/html5.py
@@ -2,10 +2,3 @@
 # flake8: noqa
 from wtforms.widgets.html5 import *
 from wtforms.fields.html5 import *
-
-from wtforms.widgets.core import Input
-
-# wtforms missing DateInput
-
-class DateInput(Input):
-    input_type = 'date'
