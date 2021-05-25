b6912943acb64d5726c8da92ddb597e212ebd2a5
David Lord <davidism@gmail.com>
2021-05-25 13:57:24 -0700

    move metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,47 +1,7 @@
-#!/usr/bin/env python
-from setuptools import find_packages, setup
-
-with open('README.rst') as f:
-    readme = f.read()
+from setuptools import setup
 
 setup(
     name='Flask-WTF',
-    version='0.15.1',
-    url='https://github.com/wtforms/flask-wtf',
-    license='BSD',
-    author='Dan Jacob',
-    author_email='danjac354@gmail.com',
-    maintainer='Hsiaoming Yang',
-    maintainer_email='me@lepture.com',
-    description='Simple integration of Flask and WTForms.',
-    long_description=readme,
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    zip_safe=False,
-    platforms='any',
-    python_requires=">= 3.6",
-    install_requires=[
-        'Flask',
-        'WTForms',
-        'itsdangerous',
-    ],
+    install_requires=['Flask', 'WTForms', 'itsdangerous'],
     extras_require={'email': ['email-validator']},
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Flask',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
 )
