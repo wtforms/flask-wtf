0fd19e5da74b9724451e842fc8502692cc9f97b1
Renato Covarrubias <rmcovarrubias@falabella.cl>
2022-03-09 22:56:16 -0300

    Apply black

--- src/flask_wtf/recaptcha/validators.py
+++ src/flask_wtf/recaptcha/validators.py
@@ -27,7 +27,7 @@ class Recaptcha:
         self.message = message
 
     def __call__(self, form, field):
-        if current_app.testing or current_app.config.get('RECAPTCHA_DISABLE', False):
+        if current_app.testing or current_app.config.get("RECAPTCHA_DISABLE", False):
             return True
 
         if request.json:
